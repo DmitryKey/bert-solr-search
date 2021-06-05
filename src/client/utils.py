1cd96510b84383ce56ebe000c85a306a69390b97
dmitry.kan <dmitry.kan@alpha-sense.com>
2021-06-05 20:10:56 +0300

    #16 I/O for binary vector format from Yandex (image dataset)

--- src/client/utils.py
+++ src/client/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import normalize
+import struct
 
 
 def get_solr_vector_search(bert_client, query):
@@ -29,7 +30,8 @@ def to_solr_vector(vectors):
 
 
 def myfmt(r):
-   return "%.10f" % (r,)
+    return "%.10f" % (r,)
+
 
 def get_elasticsearch_vector(query_vector):
     """
@@ -46,11 +48,71 @@ def get_elasticsearch_vector(query_vector):
     query_vector = np.round(query_vector.astype(np.float64), 10)
     return query_vector.flatten().tolist()
 
-    ## normalize the vector (only for GSI)
-    #print("INPUT {}".format(query_vector))
-    #query_vector = np.round(normalize(query_vector, norm='l2', axis=1), 10)
-    #print("L2 NORM + ROUND {}".format(query_vector))
-    #query_vector = query_vector.flatten('F')
-    #query_vector = np.round(query_vector, 10).tolist()
-    #print ("Flatten + list {}".format(query_vector))
-    #return query_vector
+
+"""
+                  IO Utils for reading and writing binary vectors
+"""
+
+
+def read_fbin(filename, start_idx=0, chunk_size=None):
+    """ Read *.fbin file that contains float32 vectors
+    Args:
+        :param filename (str): path to *.fbin file
+        :param start_idx (int): start reading vectors from this index
+        :param chunk_size (int): number of vectors to read.
+                                 If None, read all vectors
+    Returns:
+        Array of float32 vectors (numpy.ndarray)
+    """
+    with open(filename, "rb") as f:
+        nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
+        nvecs = (nvecs - start_idx) if chunk_size is None else chunk_size
+        arr = np.fromfile(f, count=nvecs * dim, dtype=np.float32,
+                          offset=start_idx * 4 * dim)
+    return arr.reshape(nvecs, dim)
+
+
+def read_ibin(filename, start_idx=0, chunk_size=None):
+    """ Read *.ibin file that contains int32 vectors
+    Args:
+        :param filename (str): path to *.ibin file
+        :param start_idx (int): start reading vectors from this index
+        :param chunk_size (int): number of vectors to read.
+                                 If None, read all vectors
+    Returns:
+        Array of int32 vectors (numpy.ndarray)
+    """
+    with open(filename, "rb") as f:
+        nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
+        nvecs = (nvecs - start_idx) if chunk_size is None else chunk_size
+        arr = np.fromfile(f, count=nvecs * dim, dtype=np.int32,
+                          offset=start_idx * 4 * dim)
+    return arr.reshape(nvecs, dim)
+
+
+def write_fbin(filename, vecs):
+    """ Write an array of float32 vectors to *.fbin file
+    Args:s
+        :param filename (str): path to *.fbin file
+        :param vecs (numpy.ndarray): array of float32 vectors to write
+    """
+    assert len(vecs.shape) == 2, "Input array must have 2 dimensions"
+    with open(filename, "wb") as f:
+        nvecs, dim = vecs.shape
+        f.write(struct.pack('<i', nvecs))
+        f.write(struct.pack('<i', dim))
+        vecs.astype('float32').flatten().tofile(f)
+
+
+def write_ibin(filename, vecs):
+    """ Write an array of int32 vectors to *.ibin file
+    Args:
+        :param filename (str): path to *.ibin file
+        :param vecs (numpy.ndarray): array of int32 vectors to write
+    """
+    assert len(vecs.shape) == 2, "Input array must have 2 dimensions"
+    with open(filename, "wb") as f:
+        nvecs, dim = vecs.shape
+        f.write(struct.pack('<i', nvecs))
+        f.write(struct.pack('<i', dim))
+        vecs.astype('int32').flatten().tofile(f)
